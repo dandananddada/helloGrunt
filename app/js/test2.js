f244fcbb0dd8dcf72317ed5e83da95b0b1b5156c
Eten Gstar <dandananddada@163.com>
2014-10-25 20:41:09 +0800

    A basic Grunt demo

---
+++ app/js/test2.js
@@ -0,0 +1,14 @@
+describe("Counter tests", function () {
+	it("Add gives the correct result", function () {
+        // Arrange
+        var num1 = 1;
+        var num2 = 3;
+        var expected = 4;
+ 
+        // Act
+        var result = counter.add(num1, num2);;
+ 		// var result = num1+num2;
+        // Assert
+        expect(result).toBe(expected);
+    });
+});
